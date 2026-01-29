fbc5fcda125b3ce8547b06351be3384a3ae7e5ab
CSL <caishilin@zhihu.com>
2026-01-29 11:42:58 +0800

    Initial Commit

---
+++ AiCash/AiCashApp.swift
@@ -0,0 +1,17 @@
+//
+//  AiCashApp.swift
+//  AiCash
+//
+//  Created by caishilin on 2026/1/29.
+//
+
+import SwiftUI
+
+@main
+struct AiCashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
